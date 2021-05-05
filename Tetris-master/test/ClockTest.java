740cb95ab5b684a2d2284fce720b7aac3e40dd14
jessica Shore <shore011@cougars.csusm.edu>
2021-05-04 20:41:40 -0700

    broken ass clock

--- Tetris-master/test/ClockTest.java
+++ Tetris-master/test/ClockTest.java
@@ -1,22 +1,44 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.io.PrintStream;
+import java.time.Clock;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 class ClockTest {
-
+	private static final OutputStream outContent = null;
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream ogOut = System.out;
+	private final PrintStream ogErr = System.err;
+	
+	private Clock clock;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		clock = new Clock();
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		clock = null;
+		System.setOut(ogOut);
+		System.setErr(ogErr);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void CPCtest() {
+		
+		ClockTest.clock(0);
+        assertEquals(outContent.toString().trim());
+        ((ByteArrayOutputStream) outContent).reset();
 	}
 
 }
