f2f4d0066c9cd14f73be5804895712d7b590cf81
jessica Shore <shore011@cougars.csusm.edu>
2021-05-06 18:28:43 -0700

    added to board panel test

--- Tetris-master/test/BoardPanelTest.java
+++ Tetris-master/test/BoardPanelTest.java
@@ -1,22 +1,42 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoardPanelTest {
-
+	private static final OutputStream outContent = null;
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream ogOut = System.out;
+	private final PrintStream ogErr = System.err;
+	
+	BoardPanel b;
+	Tetris t;
 	@BeforeEach
 	void setUp() throws Exception {
+		t= new Tetris();
+		b = new BoardPanel(t);
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		b = null;
+		t = null;
+		System.setOut(ogOut);
+		System.setErr(ogErr);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void clearTest() {
+		b.clear();
+		equals(null);
 	}
 
 }
