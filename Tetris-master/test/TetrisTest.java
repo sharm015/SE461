35bd3da5d5ed85a1fb709402e5dfd5113a2e9a70
jessica Shore <shore011@cougars.csusm.edu>
2021-05-06 18:21:18 -0700

    robot key functions

--- Tetris-master/test/TetrisTest.java
+++ Tetris-master/test/TetrisTest.java
@@ -1,6 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.PrintStream;
-
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.ByteArrayOutputStream;
 
 
@@ -9,13 +11,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import src.*;
+
 
 
 class TetrisTest {
 	
 	private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream ogOut = System.out;
+	private final PrintStream ogErr = System.err;
 	
 	private Tetris tetris;
 	private BoardPanel boardP;
@@ -29,6 +33,8 @@ class TetrisTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		tetris = new Tetris();
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
 		//tetris.startGame();
         //tetris.updateGame();
 		
@@ -36,20 +42,40 @@ class TetrisTest {
  
 	@AfterEach
 	void tearDown() throws Exception {
-		
+		tetris = null;
+		System.setOut(ogOut);
+		System.setErr(ogErr);
 	}
 	
-/*
+
 	@Test
-	void testTetris() {
-		fail("Not yet implemented");
+	void testKeyPressed() {
+		try {
+			Robot robot = new Robot();
+			robot.keyPress(KeyEvent.VK_S);
+			robot.keyRelease(KeyEvent.VK_S);
+			robot.keyPress(KeyEvent.VK_A);
+			robot.keyRelease(KeyEvent.VK_S);
+			robot.keyPress(KeyEvent.VK_D);
+			robot.keyRelease(KeyEvent.VK_S);
+			robot.keyPress(KeyEvent.VK_Q);
+			robot.keyRelease(KeyEvent.VK_S);
+			robot.keyPress(KeyEvent.VK_E);
+			robot.keyRelease(KeyEvent.VK_S);
+			robot.keyPress(KeyEvent.VK_P);
+			robot.keyRelease(KeyEvent.VK_S);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_S);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	void testStartGame() {
-		fail("Not yet implemented");
+		//tetris.startGame();
 	}
-*/
+
 	@Test
 	void testUpdateGame() {
 
@@ -65,5 +91,7 @@ class TetrisTest {
 		//boardP.isValidAndEmpty(TcurrentPiece, TcurrentCol, Tcurrentrow + 1 , Tcurrentrotation)
         assertNull(boardP);
         output.reset();
+        
+        //TestPath: [1,3,4,5]
 	}
 }
