add38a4304c9c1f20dbf81b52a497053808dd19c
Ria Sharma <ria.sharma24@yahoo.com>
2021-05-07 12:26:46 -0700

    tetris covers, 48.3%

--- Tetris-master/test/TetrisTest.java
+++ Tetris-master/test/TetrisTest.java
@@ -1,11 +1,11 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.PrintStream;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.ByteArrayOutputStream;
-
-
+import java.io.OutputStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,38 +16,47 @@ import org.junit.jupiter.api.Test;
 
 class TetrisTest {
 	
-	private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    private final PrintStream ogOut = System.out;
-	private final PrintStream ogErr = System.err;
+	public static final OutputStream outContent = null;
+	public final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	public final PrintStream ogOut = System.out;
+	public final PrintStream ogErr = System.err;
 	
-	private Tetris tetris;
-	private BoardPanel boardP;
-	private TileType TcurrentPiece;
-	private int TcurrentCol;
-	private int Tcurrentrow;
-	private int Tcurrentrotation;
-	boolean node1 = true;
+	public Tetris tetris;
+	public BoardPanel boardP;
+	public TileType tile1; 
+	public int OriginalRow;
+	public int TcurrentCol ;
+	public int TcurrentRotation;
 	//boardP.isValidAndEmpty(TcurrentPiece, TcurrentCol, Tcurrentrow + 1 , Tcurrentrotation);
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		tetris = new Tetris();
-		//System.setOut(new PrintStream(outContent));
-		//System.setErr(new PrintStream(errContent));
-		//tetris.startGame();
-        //tetris.updateGame();
+		boardP  = new BoardPanel(tetris); 
+		tile1 = TileType.TypeJ;
+		OriginalRow = tetris.getPieceRow();
+		TcurrentCol = tetris.getPieceCol();
+		TcurrentRotation = tetris.getPieceRotation();
 		
+		//this.boardP = new BoardPanel(this);
+		
+
 	}
  
 	@AfterEach
 	void tearDown() throws Exception {
+		//tetris = null;
+		boardP = null;
 		tetris = null;
+		tile1= null;
+		OriginalRow = 0;
+		TcurrentCol = 0;
+		TcurrentRotation = 1;
+		
 		System.setOut(ogOut);
 		System.setErr(ogErr);
 	}
-	
-
+	/*
 	@Test
 	void testKeyPressed() {
 		try {
@@ -75,22 +84,24 @@ class TetrisTest {
 	void testStartGame() {
 		//tetris.startGame();
 	}
-
+*/
 	@Test
 	void testUpdateGame() {
 
 		//----------Test path: [1,2]--------------//
-		
-		TcurrentPiece = tetris.getPieceType();
-		TcurrentCol = tetris.getPieceCol();
-		Tcurrentrow = tetris.getPieceRow();
-		Tcurrentrotation = tetris.getPieceRotation();
+		//int OriginalRow = tetris.getPieceRow();		
 		//boolean a = (this.node1 == this.node1);
         //assertTrue(a);
         //assertEquals(Tcurrentrow+2, tetris.updateGame());
-		//boardP.isValidAndEmpty(TcurrentPiece, TcurrentCol, Tcurrentrow + 1 , Tcurrentrotation)
-        assertNull(boardP);
-        output.reset();
+		
+		assertTrue(boardP.isValidAndEmpty(tile1, TcurrentCol, OriginalRow + 1 ,TcurrentRotation));
+	    
+		 //int TcurrentRow = OriginalRow + 1;
+		// assertTrue(TcurrentRow > OriginalRow);
+		 //assertEquals(2,tetris.updateGame());
+        
+		 //assertNotNull(boardP);
+        //output.reset();
         
         //TestPath: [1,3,4,5]
 	}
