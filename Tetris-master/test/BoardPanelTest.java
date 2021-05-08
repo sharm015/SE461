d3dbe33a6374076f41ccded635aba98e8cc32efc
Denise Fernandez <52894773+denise-fernandez@users.noreply.github.com>
2021-05-07 23:22:45 -0700

    boardpanel test 85%

--- Tetris-master/test/BoardPanelTest.java
+++ Tetris-master/test/BoardPanelTest.java
@@ -11,8 +11,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import src.*;
+
 class BoardPanelTest {
-	
 	public static final OutputStream outContent = null;
 	public final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 	public final PrintStream ogOut = System.out;
@@ -22,16 +23,16 @@ class BoardPanelTest {
 	public TileType ty3;
 	public BoardPanel b;
 	public Tetris t;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
-		
+
 		t= new Tetris();
 		b = new BoardPanel(t);
 		ty = TileType.TypeJ;
 		ty2=TileType.TypeI;
 		ty3 = TileType.TypeS;
-		
+
 		//System.setOut(new PrintStream(outContent));
 		//System.setErr(new PrintStream(errContent));
 	}
@@ -49,52 +50,52 @@ class BoardPanelTest {
 	void clearTest() {
 		//t.startGame();
 		//ty.getDimension();
-		
+
 		b.clear();
 		assertNull(b.tiles[0][1]);
 		//assertEquals("zero", outContent.toString().trim());
 		//outContent.flush();
 
 	}
-	
+
 	@Test
 	void isValidAndEmptyTest()
 	{
 		b.isValidAndEmpty(ty, 3, 3, 2);
-		
+		//assertEquals("true",outContent.toString().trim());
 		b.isValidAndEmpty(ty, 0, 0, 0);
-		
+		//assertEquals("true",outContent.toString().trim());
 		b.isValidAndEmpty(ty2, 4, 4, 1);
-		
+
 		b.isValidAndEmpty(ty3, 4, 4, 1);
 	}
-	
+
 	@Test
 	void addPieceTest()
 	{
-		b.addPiece(ty, 3, 3, 2);
+		//b.addPiece(ty, 3, 3, 2);
+		TileType tile2 = TileType.TypeL;
+		this.b.addPiece(tile2, 3,2,2);
+
+		assertEquals(false, this.b.isValidAndEmpty(tile2, 3,2,2));
 	}
-	
+
 	@Test
 	void checklinesTest()
 	{
-		assertEquals(22,b.checkLines());    
+		//t.board.tiles[1][1] = null;
+		assertEquals(true,this.b.checkLine(1));
+		assertEquals(22,this.b.checkLines());
+
 	}
-	
-	@Test
-	void checkLineTest()
-	{
-		assertEquals(0, b.checkLine(0));
-	}
-	
-	@Test
-	void drawTileTest()
-	{
-		b.drawTile(ty, 0, 0, null);
-	}
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 }
